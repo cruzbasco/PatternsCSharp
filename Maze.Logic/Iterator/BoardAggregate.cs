2d4f33ed2a6dc4b2e6942762b25dfa804d5f2292
Juan de la Cruz Bascope Zambrana <cruzbasco@hotmail.com>
2021-03-19 14:29:13 -0300

    remove duplicate singleton

--- Maze.Logic/Iterator/BoardAggregate.cs
+++ Maze.Logic/Iterator/BoardAggregate.cs
@@ -4,81 +4,81 @@ using System.IO;
 
 namespace Maze.Logic.Iterator
 {
-    public class BoardAggregate : IAggregate
+  public class BoardAggregate : IAggregate
+  {
+    private readonly int _size;
+    private readonly Square[,] _board;
+
+    public int Size { get { return _size; } init { } }
+
+    public BoardAggregate(int size = 10)
     {
-        private readonly int _size;
-        private readonly Square[,] _board;
+      _board = new Square[size, size];
+      _size = size;
 
-        public int Size { get { return _size; } init { } }
-
-        public BoardAggregate(int size = 10)
+      for (int r = 0; r < _size; r++)
+      {
+        for (int c = 0; c < _size; c++)
         {
-            _board = new Square[size, size];
-            _size = size;
-
-            for (int r = 0; r < _size; r++)
-            {
-                for (int c = 0; c < _size; c++)
-                {
-                    _board[r, c] = new Square();
-                }
-            }
+          _board[r, c] = new Square();
         }
-
-        public Square GetSquare(int row, int col)
-        {
-            return _board[row, col];
-        }
-
-        public void Load(string board = "Board1")
-        {
-            string directory = @"D:\code\bridgeDemo\Maze.Web";
-
-            string[] lines = File.ReadAllLines(@$"{directory}\Boards\{board}.txt");
-
-            for (int r = 0; r < _size; r++)
-            {
-                for (int c = 0; c < _size; c++)
-                {
-                    _board[r, c].Symbol = lines[r][c];
-                }
-            }
-        }
-
-        public IEnumerable<string> Print()
-        {
-            for (int r = 0; r < _size; r++)
-            {
-                var result = "";
-                for (int c = 0; c < _size; c++)
-                {
-                    //result += _board[r, c].Symbol + " " + _board[r, c].IsChecked.ToString() + " ";
-                    result += _board[r, c].Symbol;
-                }
-
-                yield return result;
-            }
-        }
-
-        public void ToLower()
-        {
-            for (int row = 0; row < _size; row++)
-            {
-                for (int col = 0; col < _size; col++)
-                {
-                    _board[row, col].ToLower();
-                }
-            }
-        }
-
-        public IAbstractIterator CreateIterator(IteratorType iteratorType)
-        {
-            return iteratorType switch
-            {
-                IteratorType.CrossIterator => new CrossIterator(_board, _size),
-                IteratorType.DiagonalIterator => new DiagonalIterator(_board, _size),
-                _ => new FullIterator(_board, _size)
-            };
-        }
+      }
     }
+
+    public Square GetSquare(int row, int col)
+    {
+      return _board[row, col];
+    }
+
+    public void Load(string board = "Board1")
+    {
+      string directory = @"D:\code\bridgeDemo\Maze.Web";
+
+      string[] lines = File.ReadAllLines(@$"{directory}\Boards\{board}.txt");
+
+      for (int r = 0; r < _size; r++)
+      {
+        for (int c = 0; c < _size; c++)
+        {
+          _board[r, c].Symbol = lines[r][c];
+        }
+      }
+    }
+
+    public IEnumerable<string> Print()
+    {
+      for (int r = 0; r < _size; r++)
+      {
+        var result = "";
+        for (int c = 0; c < _size; c++)
+        {
+          //result += _board[r, c].Symbol + " " + _board[r, c].IsChecked.ToString() + " ";
+          result += _board[r, c].Symbol;
+        }
+
+        yield return result;
+      }
+    }
+
+    public void ToLower()
+    {
+      for (int row = 0; row < _size; row++)
+      {
+        for (int col = 0; col < _size; col++)
+        {
+          _board[row, col].ToLower();
+        }
+      }
+    }
+
+    public IAbstractIterator CreateIterator(IteratorType iteratorType)
+    {
+      return iteratorType switch
+      {
+        IteratorType.CrossIterator => new CrossIterator(_board, _size),
+        IteratorType.DiagonalIterator => new DiagonalIterator(_board, _size),
+        _ => new FullIterator(_board, _size)
+      };
+    }
+  }
 }
