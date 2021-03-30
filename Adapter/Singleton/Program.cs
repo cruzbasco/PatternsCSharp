1514f43a4737d308d0bf827dfa12c78f206e228f
Juan de la Cruz Bascope Zambrana <cruzbasco@hotmail.com>
2021-03-30 09:45:19 -0300

    problem with the singleton project
    delete project and recreate

---
+++ Adapter/Singleton/Program.cs
@@ -0,0 +1,79 @@
+﻿#nullable enable
+using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var a1 = new ClassA("a1");
+            var a2 = new ClassA("a2");
+            var a3 = new ClassA("a3");
+
+            var c1 = new ClassC("c1");
+            var c2 = new ClassC("c2");
+
+            var b1 = new ClassB("b1");
+
+            var config = Config.Instance();
+            Console.WriteLine(config.Message);
+
+        }
+
+    }
+
+
+
+
+    public class Config
+    {
+        private static readonly Config _instance = new();
+
+        public string Message { get; set; } = "";
+
+        private Config()
+        {
+        }
+
+        public static Config Instance()
+        {
+            return _instance;
+        }
+
+    }
+
+
+
+    public class ClassA
+    {
+        private readonly Config config = Config.Instance();
+
+        public ClassA(string message)
+        {
+            config.Message += message;
+        }
+    }
+
+
+    public class ClassB
+    {
+        private readonly Config config = Config.Instance();
+
+        public ClassB(string message)
+        {
+            config.Message += message;
+        }
+    }
+    public class ClassC
+    {
+        private readonly Config config = Config.Instance();
+
+        public ClassC(string message)
+        {
+            config.Message += message;
+        }
+    }
+
+}
